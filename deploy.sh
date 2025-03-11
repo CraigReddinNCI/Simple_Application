de7656381175c70e75f4cce1ace96329bb0e48a5
Craig Reddin <craig-reddin@hotmail.com>
2025-03-11 21:12:24 +0000

    Test after CICD working - Video audition

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Stop and remove the old container if running
 sudo pm2 stop simple_app  || true
+sudo pm2 delete simple_app
 sudo docker stop simple_app || true
 sudo docker rm simple_app || true
 
