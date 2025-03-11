4501b53752d2cf56fdfe20aec6c598827f5e8a5c
Craig Reddin <craig-reddin@hotmail.com>
2025-03-11 18:54:46 +0000

    Test after CICD working

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@
 sudo docker stop simple_app || true
 sudo docker rm simple_app || true
 
-sudo docker image prune -a -f && \
-sudo docker pull $IMAGE_APP:latest && \
-
-# Run the new container in background.
+# Run the new container
 sudo docker run -d \
     -p 8443:8443 \
   --name simple_app \
