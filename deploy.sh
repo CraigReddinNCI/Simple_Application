0b61a7711d78e760ec5ac99dcfeb4bfca7482685
Craig Reddin <craig-reddin@hotmail.com>
2025-03-12 00:40:40 +0000

    Test after CICD working - Video audition

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 # Stop and remove the old container if running
 sudo docker stop simple_app || true
 sudo docker rm simple_app || true
-
-   # Ensure we get the latest version
+sudo docker rmi -f $IMAGE_APP:latest  # Remove the old image
+sudo docker pull $IMAGE_APP:latest    # Ensure we get the latest version
 
 # Run the
 sudo docker run -d \
     -p 8443:8443 \
   --name simple_app \
+  --pull always \
   $IMAGE_APP
